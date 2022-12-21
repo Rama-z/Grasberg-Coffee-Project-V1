9e0584552296383400e34b193280a25768d1d294
Zanuar Bagus Ramadhan <zanuarbagusramadhan@gmail.com>
2022-12-21 14:51:48 +0700

    edit profile

--- src/repo/products.js
+++ src/repo/products.js
@@ -87,7 +87,7 @@ module.exports = {
   getProductById: (id) => {
     return new Promise((resolve, reject) => {
       const query =
-        "select p.id, p.menu, p.price, p.image, c.category_name, p.description from products p join categorize c on c.id = p.varian_id left join transactions t on t.product_id = p.id where p.id = $1 group by p.id, menu, p.price , p.image, c.category_name , description";
+        "select p.id, p.menu, p.price, p.image, c.category_name, p.description, p2.discount from products p join categorize c on c.id = p.varian_id left join transactions t on t.product_id = p.id join promos p2 on p2.id = p.promo_id where p.id = $1 group by p.id, menu, p.price , p.image, c.category_name , description";
       database.query(query, [id], (err, result) => {
         if (err) {
           return reject({
